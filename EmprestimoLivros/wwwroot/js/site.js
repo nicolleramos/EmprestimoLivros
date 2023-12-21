d205f0784279d33e3d194c0411a146052d2becfc
Pichau <Pichau@DESKTOP-E75284G>
2023-12-21 13:30:33 -0300

    Adicionar arquivos de projeto.

---
+++ EmprestimoLivros/wwwroot/js/site.js
@@ -0,0 +1,39 @@
+﻿
+$(document).ready(function () {
+
+    $('#Emprestimos').DataTable({
+        language:
+        {
+            "decimal": "",
+            "emptyTable": "No data available in table",
+            "info": "Mostrando _START_ registro de _END_ em um total de _TOTAL_ entradas",
+            "infoEmpty": "Showing 0 to 0 of 0 entries",
+            "infoFiltered": "(filtrado por total de _MAX_ registros)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Exibir _MENU_ entradas",
+            "loadingRecords": "Loading...",
+            "processing": "",
+            "search": "Pesquisar: ",
+            "zeroRecords": "No matching records found",
+            "paginate": {
+                "first": "Primeiro",
+                "last": "Último",
+                "next": "Próximo",
+                "previous": "Anterior"
+            },
+            "aria": {
+                "sortAscending": ": activate to sort column ascending",
+                "sortDescending": ": activate to sort column descending"
+            }
+        }
+    });
+
+    setTimeout(function () {
+        $(".alert").fadeOut("slow", function () {
+            $(this).alert('close');
+        })
+    }, 5000)
+
+});
+
